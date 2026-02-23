e2cb02352dbe472af12dcfa11481998b2c7359c5
Mimansa Joshi <mimansaj@vconstruct.in>
2026-02-23 13:06:26 +0530

    update: restructured folder and installed libraries

--- contexts/DBContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-import { getDB } from "../database/schema/database.ts";
-
-const db = await getDB();
-
-const DBContext = createContext(db);
-
-export default DBContext;
